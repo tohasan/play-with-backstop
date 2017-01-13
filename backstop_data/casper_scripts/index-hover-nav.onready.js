3f62b05f143b56a2bae3ca0417610598a8f2a550
tohasan <tohasan@yandex.ru>
2017-01-13 12:06:57 +0300

    step 2, user interactions: changed name of onready script

--- backstop_data/casper_scripts/index-hover-nav.onready.js
+++ backstop_data/casper_scripts/index-hover-nav.onready.js
@@ -2,9 +2,9 @@ module.exports = function (casper, scenario, vp) {
     casper.then(function () {
         var navLinkSelector = '.nav-link[data-action="contact"]';
         casper.waitUntilVisible(navLinkSelector, function () {
-            console.log('onReady.js: nav-link visible'); // NOSONAR
+            console.log('index-hover-nav.onready.js: nav-link visible'); // NOSONAR
             casper.mouse.move(navLinkSelector);
         });
     });
-    console.log('onReady.js has run for: ', vp.name); // NOSONAR
+    console.log('index-hover-nav.onready.js has run for: ', vp.name); // NOSONAR
 };
