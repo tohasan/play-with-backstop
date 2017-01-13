a8259501372cea33e74596612357ca7916dc6e79
tohasan <tohasan@yandex.ru>
2017-01-13 11:53:03 +0300

    step 2, user interactions: added support for hover

--- backstop_data/casper_scripts/onReady.js
+++ backstop_data/casper_scripts/onReady.js
@@ -1,6 +1,10 @@
 module.exports = function (casper, scenario, vp) {
-  casper.evaluate(function () {
-    //Your web-app is now loaded. Edit here to simulate user interacions or other state changes.
-  });
-  console.log('onReady.js has run for: ', vp.name);
+    casper.then(function () {
+        var navLinkSelector = '.nav-link[data-action="contact"]';
+        casper.waitUntilVisible(navLinkSelector, function () {
+            console.log('onReady.js: nav-link visible'); // NOSONAR
+            casper.mouse.move(navLinkSelector);
+        });
+    });
+    console.log('onReady.js has run for: ', vp.name); // NOSONAR
 };
